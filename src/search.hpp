db5b6f29b79e7a14d71080a0b032e05357b6da5f
NandeMD <celikumutcan2001@gmail.com>
2026-04-10 23:02:28 +0300

    Change to pointer passing instead of pbr.
    This change is needed because we will need to pass a pointer to the sort
    function, and inside that sort function since we have a pointer, then
    passing by reference seems wrong.

--- src/search.hpp
+++ src/search.hpp
@@ -5,4 +5,4 @@
 #define usize std::uintptr_t
 #define uint std::int32_t
 
-usize b_src_on_array(uint (&arr)[], usize n, uint element, usize upper_bound);
+usize b_src_on_array(uint *arr, usize n, uint element, usize upper_bound);
