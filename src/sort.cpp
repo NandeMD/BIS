63805173f8d5eb2d617e345cba5d613e16d712a9
NandeMD <celikumutcan2001@gmail.com>
2026-04-12 17:58:45 +0300

    finalise the sort

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,3 +1,22 @@
 #ifndef SORT
 #include "sort.hpp"
 #endif
+
+void binary_insertion_sort(uint *arr, usize n) {
+    for (usize i = 1; i < n; ++i) {
+        usize cursor = arr[i];
+
+        usize needs_to_be_at = b_src_on_array(arr, i, cursor, i - 1);
+
+        // Shift all elements before the cursor
+        // to 1 place right, starting from the right
+        // so we don't lose items
+        for (usize j = i - 1; j >= needs_to_be_at; --j) {
+            arr[j + 1] = arr[j];
+        }
+
+        // And finally place the pointed item
+        // to it's rightful place
+        arr[needs_to_be_at] = cursor;
+    }
+}
