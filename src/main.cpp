bfc9c15e0363e8945497e52547f590346f3caeae
NandeMD <celikumutcan2001@gmail.com>
2026-04-12 18:00:53 +0300

    new test for even sized arrays

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,13 @@ int main() {
     std::cout << std::endl << "After:" << std::endl;
     print_arr(arr, 5, std::string("arr"));
 
+    uint arr2[] = {3, 8, 5, 4, 100, 50};
+    std::cout << "Before:" << std::endl;
+    print_arr(arr2, 6, std::string("arr"));
+
+    binary_insertion_sort(arr2, 6);
+    std::cout << std::endl << "After:" << std::endl;
+    print_arr(arr2, 6, std::string("arr"));
+
     return 0;
 }
