4fca416b2b34269b33cfc9477cd667616ed4ea90
NandeMD <celikumutcan2001@gmail.com>
2026-04-12 17:57:26 +0300

    small test case

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+
+#include "printer.hpp"
 #include "search.hpp"
 #include "sort.hpp"
 
-int main() { return 0; }
+int main() {
+    uint arr[] = {3, 8, 5, 4, 100};
+    std::cout << "Before:" << std::endl;
+    print_arr(arr, 5, std::string("arr"));
+
+    binary_insertion_sort(arr, 5);
+    std::cout << std::endl << "After:" << std::endl;
+    print_arr(arr, 5, std::string("arr"));
+
+    return 0;
+}
